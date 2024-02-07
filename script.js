e34a2ff8733cdb87062aea4e90fb96ac9b91ee5f
Alexander Kozlov <alex-k@akozlov.lan>
2024-02-07 18:54:25 -0500

    added ability to hande keyboard inputs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const display = document.querySelector(".display");
 display.innerHTML = "0";
+window.addEventListener('keydown', handleKeyboardInput)
 
 function appendNumber(number) {
     if (display.innerHTML === "0") {
@@ -37,3 +38,10 @@ function calculate() {
     display.innerHTML = result;
 }
 
+function handleKeyboardInput(input) {
+    if (input.key >= 0 && input.key <= 9 || input.key === '.'   ) appendNumber(input.key)
+    if (input.key === '=' || input.key === 'Enter') calculate()
+    if (input.key === 'Escape') clearDisplay()
+    if (input.key === '+' || input.key === '-' || input.key === '*' || input.key === '/')
+    appendOperator(input.key);
+}
